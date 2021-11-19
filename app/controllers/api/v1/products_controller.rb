60c008867a7598c34eeca9392a94aaffecf7f6d7
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-11-20 02:17:31 +0900

    初期ルーティング、product_index
    cors

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::ProductsController < ApplicationController
+  def index
+    @products = Product.all.where(finished:0).limit(30)
+    render json: { status: 200, message: "Hello World!",products: @products}
+  end
 end

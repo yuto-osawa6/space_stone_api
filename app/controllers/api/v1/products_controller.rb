5788cb9ff1bdb8d4add34e9e164b195008aaa966
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-11-26 16:32:13 +0900

    お試し

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::ProductsController < ApplicationController
     @products = Product.all.where(finished:0).limit(30)
     render json: { status: 200, message: "Hello World!",products: @products}
   end
+
+  def red
+    @products = Product.all.where(finished:0).limit(30)
+    render json: { status: 200, message: "Hello World!43",products: @products}
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :products
+      resources :products do
+        collection do
+          get "red"
+        end
+      end
     end
   end
+
 end

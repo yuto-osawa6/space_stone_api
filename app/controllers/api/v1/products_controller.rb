6d3f981f6513a56df9a67ee64518c54d8b87506b
yuto-osawa6 <pooh.atgmw628@gmail.com>
2021-11-26 17:17:52 +0900

    layout_left

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::ProductsController < ApplicationController
+
+  def index
+    @products = Product.all.where(finished:0).limit(30)
+    render json: { status: 200, message: "Hello World!",products: @products,styles:@styles}
+  end
+
+  def red
+    @products = Product.all.where(finished:0).limit(30)
+    render json: { status: 200, message: "Hello World!43",products: @products}
+    
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :first
+
+  def first
+    @styles = Style.all
+    @genre = Janl.all
+    # render json: { status: 201, message: "Hello World!",styles: @styles}
+
+  end
 end

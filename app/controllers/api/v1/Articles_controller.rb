0ad217432b7816c73d800432054eae5a48c392c4
yuto-osawa6 <pooh.atgmw628@gmail.com>
2022-02-10 23:16:15 +0900

    n+1 article show

--- app/controllers/api/v1/Articles_controller.rb
+++ app/controllers/api/v1/Articles_controller.rb
@@ -32,14 +32,16 @@ class Api::V1::ArticlesController < ApplicationController
     end
 
     def associate
-      @product = Product.find(params[:product_id])
+      @product = Product.includes(:styles,:janls).find(params[:product_id])
       render :associate,formats: :json
     end
 
     def article_associate
       article = ArticleProduct.where(article_id:params[:article_id]).pluck(:product_id)
       puts article
-      @articles = Article.joins(:article_products).where(article_products: { product_id: article }).group(:article_id).order("count(article_id) desc").limit(6)
+      # @articles = Article.joins(:article_products).where(article_products: { product_id: article }).group(:article_id).order("count(article_id) desc").limit(6)
+      @articles = Article.left_outer_joins(:products).includes(:products).where(article_products: { product_id: article }).group(:article_id).order("count(article_id) desc").limit(6)
+
       # product_ids = article.group(:product_id).plunk(:product_id)
       render :article_associate,formats: :json
     end

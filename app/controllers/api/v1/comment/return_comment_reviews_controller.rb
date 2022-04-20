09b628b28262a1ccf69b6f1479314df485dc9df1
yuto-osawa6 <pooh.atgmw628@gmail.com>
2022-04-20 10:30:07 +0900

    return comment test

--- app/controllers/api/v1/comment/return_comment_reviews_controller.rb
+++ app/controllers/api/v1/comment/return_comment_reviews_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::Comment::ReturnCommentReviewsController < ApplicationController
   def index
-    params[:comment_review_id]
+    # params[:comment_review_id]
     # userは対１関係なため、includesに含めない。
-    # @review = CommentReview.includes(:like_comment_reviews).find(params[:comment_review_id])
     @returncomment = ReturnCommentReview.includes(:like_return_comment_reviews,:user,return_returns: :user).where(comment_review_id:params[:comment_review_id]).page(params[:page]).per(3)
     render :index,formats: :json
   end
@@ -14,7 +13,7 @@ class Api::V1::Comment::ReturnCommentReviewsController < ApplicationController
       render json: {status:200,commentReview:@commentReview}
     rescue => e
       if Review.exists?(id:params[:review_id])
-        if CommentReview.exists?(id:params[:comment_review_id])
+        if CommentReview.exists?(id:params[:return_comment_review][:comment_review_id])
           @EM = ErrorManage.new(controller:"return_comment_review/create",error:"#{e}".slice(0,200))
           @EM.save
           render json: {status:500}
@@ -53,8 +52,6 @@ class Api::V1::Comment::ReturnCommentReviewsController < ApplicationController
   end
 
   def destroy
-    puts params
-    # {"review_id"=>"62", "comment_review_id"=>59, "id"=>"105", "return_comment_review"=>{"comment_review_id"=>59}}
     begin
       @review_comment = ReturnCommentReview.find(params[:id])
       @review_comment.destroy
@@ -63,13 +60,9 @@ class Api::V1::Comment::ReturnCommentReviewsController < ApplicationController
       if Review.exists?(id:params[:review_id])
         if CommentReview.exists?(id:params[:comment_review_id])
           if ReturnCommentReview.exists?(id:params[:id])
-            # if ReturnCommentReview.exists?(id:params[:return_return_comment_review][:return_return_id])
-              @EM = ErrorManage.new(controller:"return_comment_review/destroy",error:"#{e}".slice(0,200))
-              @EM.save
-              render json: {status:500}
-            # else
-            #   render json: {status:420}
-            # end
+            @EM = ErrorManage.new(controller:"return_comment_review/destroy",error:"#{e}".slice(0,200))
+            @EM.save
+            render json: {status:500}
           else
             render json: {status:420}
           end
@@ -85,11 +78,9 @@ class Api::V1::Comment::ReturnCommentReviewsController < ApplicationController
   private
   def create_params
     params.require(:return_comment_review).permit(:user_id,:comment_review_id,:comment)
-    # params.require(:like).permit(:product_id,:user_id,:review_id,:content)
   end
   def create_params2
     params.require(:return_comment_review).permit(:user_id,:comment_review_id,:comment).merge(reply:true)
-    # params.require(:like).permit(:product_id,:user_id,:review_id,:content)
   end
 
   def return_create_params
---
+++ spec/requests/api/v1/comments/return_comment_threads_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe 'return_comment_threads', type: :request do
+  describe 'get/index' do
+    let!(:product) {create(:product_alice_thread_all)}
+    subject { get "/api/v1/comment/return_comment_threads",params: {comment_thread_id:CommentThread.first.id,page:1}}
+    it 'ステータス 200' do
+      subject
+      expect(json['status']).to eq(200)
+    end
+  end
+
+
+  describe 'post/create' do
+    let!(:product) {create(:product_alice_thread_all)}
+    context '正常' do
+      subject { post "/api/v1/comment/return_comment_threads",params: {thread_id:Thered.first.id,comment_thread_id:CommentThread.first.id,return_comment_thread: {user_id:User.first.id,comment_thread_id:CommentThread.first.id,comment:"test"}}}
+      it 'ステータス 200' do
+        subject
+        expect(json['status']).to eq(200)
+      end
+    end
+    context '異常' do
+      it 'ステータス 400(Threadが存在しない)' do
+        post "/api/v1/comment/return_comment_threads",params: {thread_id:0,comment_thread_id:0,return_comment_thread: {user_id:User.first.id,comment_thread_id:0,comment:"test"}}
+        expect(json['status']).to eq(400)
+      end
+      it 'ステータス 410(CommentThreadが存在しない)' do
+        post "/api/v1/comment/return_comment_threads",params: {thread_id:Thered.first.id,comment_thread_id:0,return_comment_thread: {user_id:User.first.id,comment_thread_id:0,comment:"test"}}
+        expect(json['status']).to eq(410)
+      end
+    end
+  end
+
+  describe 'delete/destroy' do
+    let!(:product) {create(:product_alice_thread_all)}
+    context '正常' do
+      subject { delete "/api/v1/comment/return_comment_threads/#{ReturnCommentThread.first.id}",params: {thread_id:Thered.first.id,comment_thread_id:CommentThread.first.id}}
+      it 'ステータス 200' do
+        subject
+        expect(json['status']).to eq(200)
+      end
+    end
+    context '異常' do
+      it 'ステータス 400(Threadが存在しない)' do
+        delete "/api/v1/comment/return_comment_threads/#{0}",params: {thread_id:0,comment_thread_id:0}
+        expect(json['status']).to eq(400)
+      end
+      it 'ステータス 410(CommentThreadが存在しない)' do
+        delete "/api/v1/comment/return_comment_threads/#{0}",params: {thread_id:Thered.first.id,comment_thread_id:0}
+        expect(json['status']).to eq(410)
+      end
+      it 'ステータス 420(ReturnCommentThreadが存在しない)' do
+        delete "/api/v1/comment/return_comment_threads/#{0}",params: {thread_id:Thered.first.id,comment_thread_id:CommentThread.first.id}
+        expect(json['status']).to eq(420)
+      end
+    end
+  end
+
+  describe 'post/returnreturn' do
+    let!(:product) {create(:product_alice_thread_all)}
+    context '正常' do
+      subject { post "/api/v1/comment/return_comment_threads/returnreturn",params: {thread_id:Thered.first.id,return_comment_thread: {user_id:User.first.id,comment_thread_id:CommentThread.first.id,comment:"test"},return_return_comment_thread:{return_return_thread_id:ReturnCommentThread.first.id}}}
+      it 'ステータス 200' do
+        subject
+        expect(json['status']).to eq(200)
+      end
+    end
+    context '異常' do
+      it 'ステータス 400(threadが存在しない)' do
+        post "/api/v1/comment/return_comment_threads/returnreturn",params: {thread_id:0,return_comment_thread: {user_id:User.first.id,comment_thread_id:0,comment:"test"},return_return_comment_thread:{return_return_thread_id:0}}
+        expect(json['status']).to eq(400)
+      end
+      it 'ステータス 410(Commentthreadが存在しない)' do
+        post "/api/v1/comment/return_comment_threads/returnreturn",params: {thread_id:Thered.first.id,return_comment_thread: {user_id:User.first.id,comment_thread_id:0,comment:"test"},return_return_comment_thread:{return_return_thread_id:0}}
+        expect(json['status']).to eq(410)
+      end
+      it 'ステータス 430(ReturnCommentthread(return_return_id)が存在しない)' do
+        post "/api/v1/comment/return_comment_threadthreads/returnreturn",params: {thread_id:Thered.first.id,return_comment_thread: {user_id:User.first.id,comment_thread_id:CommentThreadThread.first.id,comment:"test"},return_return_comment_thread:{return_return_thread_id:0}}
+        expect(json['status']).to eq(430)
+      end
+    end
+  end
+end

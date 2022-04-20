1aaaf6e0021d61b2e7fded088beea652f39a1759
yuto-osawa6 <pooh.atgmw628@gmail.com>
2022-04-21 06:19:56 +0900

    end test

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,15 +1,87 @@
-# require 'rails_helper'
-# # binding.pry
+require 'rails_helper'
+# binding.pry
 
-# RSpec.describe '', type: :request do
-#   describe 'GET /left' do
-#     subject { get '/api/v1/products/left' }
-#     let!(:product_left) {create_list(:product_left,10)}
-#     # let!(:style) {create_list(:style,3)}
-#     # let!(:janl) {create_list(:janl,3)}
-#     it 'ステータス 200' do
-#       subject
-#       expect(response.status).to eq(200)
-#     end
-#   end
-# end
+RSpec.describe '', type: :request do
+  describe 'patch/setting' do
+    let!(:user) {create(:user)}
+    it 'ステータス 200' do
+      patch '/api/v1/users/setting',params:{
+        user_id:User.first.id,
+        nickname:"aiueo",
+        user:{
+          user_id:User.first.id
+        }
+      }
+      expect(json['status']).to eq(200)
+    end
+  end
+  describe 'patch/background' do
+    let!(:user) {create(:user)}
+    it 'ステータス 200' do
+      patch '/api/v1/users/background',params:{
+        user_id:User.first.id,
+        background_image: "background",
+        user:{
+          user_id:User.first.id
+        }
+      }
+      expect(json['status']).to eq(200)
+    end
+  end
+  describe 'patch/overview' do
+    let!(:user) {create(:user)}
+    it 'ステータス 200' do
+      patch '/api/v1/users/overview',params:{
+        user_id:User.first.id,
+        overview:"overview",
+        user:{
+          user_id:User.first.id
+        }
+      }
+      expect(json['status']).to eq(200)
+    end
+  end
+  describe 'get/show' do
+    let!(:product) {create(:product_alice)}
+    it 'ステータス 200' do
+      get "/api/v1/users/#{User.first.id}",params:{
+        user_id:User.first.id
+      }
+      expect(json['status']).to eq(200)
+    end
+  end
+  describe 'get/likes' do
+    let!(:product) {create(:product_alice)}
+    it 'ステータス 200' do
+      get "/api/v1/users/likes",params:{
+        user_id:User.first.id
+      }
+      expect(response.status).to eq(200)
+    end
+  end
+  describe 'get/likeGenres' do
+    let!(:product) {create(:product_alice)}
+    it 'ステータス 200' do
+      get "/api/v1/users/likeGenres",params:{
+        user_id:User.first.id
+      }
+      expect(response.status).to eq(200)
+    end
+  end
+  describe 'get/mytiers' do
+    let!(:product) {create(:product_alice_tier)}
+    it 'ステータス 200' do
+      get "/api/v1/users/mytiers",params:{
+        user_id:User.first.id
+      }
+      expect(response.status).to eq(200)
+    end
+  end
+  describe 'delete/destroy' do
+    let!(:user) {create(:user)}
+    it 'ステータス 200' do
+      delete "/api/v1/users/#{User.first.id}"
+      expect(response.status).to eq(200)
+    end
+  end
+end

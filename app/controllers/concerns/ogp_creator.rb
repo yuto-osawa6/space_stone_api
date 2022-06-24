306ad9ff1582186785a78d09e94b0b472c055045
yuto-osawa6 <62257152+yuto-osawa6@users.noreply.github.com>
2022-06-25 07:41:15 +0900

    ogp

--- app/controllers/concerns/ogp_creator.rb
+++ app/controllers/concerns/ogp_creator.rb
@@ -1,34 +1,35 @@
 class OgpCreator
   require 'mini_magick'  
-  BASE_IMAGE_PATH = './public/MeruPlanetOgp2.png'
+  BASE_IMAGE_PATH = './public/MeruPlanetG/merupla-ogp2.png'
   # 3か1
   SHADOW_IMAGE_PATH2 = './public/background/MeruPlanetBack_half_trans3.png'
   SHADOW_IMAGE_PATH3 = "./public/waku/merupla-waku.png"
   GRAVITY = 'northwest'
   TEXT_POSITION = '50,95'
-  FONT = './public/Noto_Sans_JP/NotoSansJP-Medium.otf'
+  # FONT = './public/Noto_Sans_JP/NotoSansJP-Medium.otf'
+  FONT = './public/font/MPLUSRounded1c-Medium.ttf'
   FONT_SIZE = 60
   INDENTION_COUNT = 18
   ROW_LIMIT = 2
-  TEXT_POSITION2 = '50,270'
+  TEXT_POSITION2 = '50,280'
   # 50 95 183 270 359
 
   def self.build(image_url,product,average)
     text = prepare_text("#{product.title}")
     # image_url1 = image_url.blank?? BASE_IMAGE_PATH : image_url
-    image = MiniMagick::Image.open(BASE_IMAGE_PATH)
-    # result.combine_options do |config|        
-    #   config.blur "50,50"
+    result = MiniMagick::Image.open(BASE_IMAGE_PATH)
+    # image.combine_options do |config|        
+    #   # config.blur 50
     # end
-    result1 = image.composite(MiniMagick::Image.open(SHADOW_IMAGE_PATH2)) do |config|
-      config.compose 'Over'
-      config.gravity 'NorthWest'
-    end
-    result = result1.composite(MiniMagick::Image.open(SHADOW_IMAGE_PATH3)) do |config|
-      config.compose 'Over'
-      config.gravity 'NorthWest'
-      config.geometry '+500+260'
-    end
+    # result1 = image.composite(MiniMagick::Image.open(SHADOW_IMAGE_PATH2)) do |config|
+    #   config.compose 'Over'
+    #   # config.gravity 'NorthWest'
+    # end
+    # result = result1.composite(MiniMagick::Image.open(SHADOW_IMAGE_PATH3)) do |config|
+    #   config.compose 'Over'
+    #   # config.gravity 'NorthWest'
+    #   config.geometry '+500+260'
+    # end
     result.combine_options do |config|        
       config.font FONT
       config.fill '#f0f8ff'
@@ -57,14 +58,14 @@ class OgpCreator
     end
     # # product.studios.map
 
-    # result.combine_options do |config|        
-    #   config.font FONT
-    #   config.fill '#38B6FF'
-    #   config.gravity GRAVITY
-    #   config.pointsize 30
-    #   config.draw "text 50,370 '#{product.studios.map { |user| user.company }.join(',')}'"
-    #   # users.map { |user| user.name }
-    # end
+    result.combine_options do |config|        
+      config.font FONT
+      config.fill '#ff3073'
+      config.gravity GRAVITY
+      config.pointsize 30
+      config.draw "text 50,460 '#{product.studios.map { |user| user.company }.join(',')}'"
+      # users.map { |user| user.name }
+    end
   end
 
   private
